c26b706e5069e7e9b268784639aedfafc84b7852
Jigish Patel <jigish.patel@gmail.com>
2017-01-01 13:43:36 -0800

    update to foundation settings and webpack config to make it work with Foundation 6.3

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
-    'script!foundation-sites/dist/foundation.min.js',
+    'script!foundation-sites/dist/js/foundation.min.js',
     './app/app.jsx'
   ],
   externals: {
